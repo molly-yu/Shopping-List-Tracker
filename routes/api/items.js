59320075ea7eb780ace27ccd473f78ac4ce6275b
molly-yu <55213953+molly-yu@users.noreply.github.com>
2020-05-12 23:19:30 -0400

    Created Item Schema, route

---
+++ routes/api/items.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc Get all items
+// @access Public
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+
+// Use routes
+app.use('.api/items', items);
+
+//DB 
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors()); // middleware
+app.use(express.json()); 
+
+const uri = process.env.ATLAS_URI; // database uri from mongoDB
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully.");
+})
+
+app.listen(port, () => console.log('Server started on port ${port}'));
