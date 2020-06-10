aee113e703625013f3241231df4e9a7a2c0868f3
molly-yu <55213953+molly-yu@users.noreply.github.com>
2020-06-09 22:13:16 -0400

    implemented redux

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,9 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENTION__()
-));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(...middleware)
+  ));
 
 export default store;
