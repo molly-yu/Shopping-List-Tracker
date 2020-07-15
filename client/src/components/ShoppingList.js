45503567f51a626fcb36c31cb40f63838fb8a9c6
Molly Yu <m234yu@uwaterloo.ca>
2020-07-15 11:29:42 -0400

    Update ShoppingList.js

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import uuid from 'react-uuid';
 import {connect} from 'react-redux';
 import {getItems, deleteItem} from '../actions/itemActions';
 import PropTypes from 'prop-types';
